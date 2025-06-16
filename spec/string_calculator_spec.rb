8ec02f6e55244c6d148d1e115c83461493e85542
yamunanollu <yamuna.nollu@icentris.com>
2025-06-16 10:19:57 +0530

    add method to cal sum

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,10 @@
 class StringCalculator
   def add(str)
-    0
+   if str.nil?
+      0
+   else
+      str.split(',').map(&:to_i).sum
+   end
   end
 end
 
@@ -10,5 +14,17 @@ RSpec.describe StringCalculator do
     string = StringCalculator.new
     result = string.add("")
     expect(result).to eq(0)
+  end
+
+  it 'calling add method with 1' do
+    string = StringCalculator.new
+    result = string.add("1")
+    expect(result).to eq(1)
+  end
+
+  it 'calling add method with 1,5' do
+    string = StringCalculator.new
+    result = string.add("1,5")
+    expect(result).to eq(6)
   end
 end
